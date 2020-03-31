c0cd63b69a21863e5001242799fca378d2da6a15
ghost-guest <2661471268@qq.com>
2020-03-31 21:53:07 +0800

    a

--- seaborn_analysis/t2.py
+++ seaborn_analysis/t2.py
@@ -6,3 +6,21 @@
 # @File    : t2.py
 # @Software: PyCharm
 # 说明： 
+import scipy
+from scipy import constants
+import numpy as np
+from scipy import linalg
+
+print(constants.pi)
+print(constants.golden)
+print(constants.c)
+print(constants.speed_of_light)
+print(constants.h)
+print(constants.Planck)
+
+U, s, Vh = linalg.svd(np.random.randn(5, 4))
+print(U)
+print(s)
+print(Vh)
+
+
